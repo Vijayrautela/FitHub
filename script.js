d31247cf39a4099728f29b4981fa89ce5865bbbc
Vijayrautela <vrautela87@gmail.com>
2025-07-07 14:23:36 +0530

    add loading screen

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ function togglemenu(){
     const nav=document.getElementById("navlink");
     nav.classList.toggle("show");
 }
+
+// loading screen
+window.addEventListener("load",function(){
+    this.setTimeout(function(){
+         const loader= document.getElementById("loading-screen");
+   const main = document.getElementById("main-contant");
+   loader.classList.add("hidden");
+   main.style.opacity="1";
+    },2000);
+  
+});
